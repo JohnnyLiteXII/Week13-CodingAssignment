16309d02998a1c4fa68f56dc0a9344bfc21ee017
Mejia <juancme@leeschools.net>
2023-10-28 23:27:02 -0400

    Added Instructions

--- form/src/App.js
+++ form/src/App.js
@@ -1,3 +1,19 @@
+/*-- Copyright (c) 2023 Promineo Tech 
+
+Author:  Juan Mejia
+
+Week 13 - Coding Assignment
+
+----------------------------------------------------
+
+Coding Steps:
+Using create-react-app, create a new React project. Create a LoginForm component that contains the following:
+- Username and password input fields, an h3 that says Log In, with a border. Style the component using the default generated CSS file.
+Create a Navigation component that contains links styled like a navbar.
+- The links don't have to go anywhere.
+Put the Navigation component at the top of the page and the LoginForm in the center of the page.
+*/
+
 import './App.css';          // Import the global CSS file for styling
 
 import Navbar from './components/Navbar.js';      // Import the Navbar component
@@ -15,3 +31,5 @@ function App() {
 }
 
 export default App;       // Export the App component as the default export
+
+
